48cf393d02f3ada28faf5f3700cc1839ef2ba91d
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-07-13 21:10:49 -0400

    Add placeholder animation and enemy

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     Rigidbody2D rb;
-    float speed = 3f;
+    Animator animator;
     Vector2 movement;
+    float speed = 3f;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        animator.SetBool("Grounded", true);
     }
 
     private void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        Animate();
+        Flip();
     }
 
     private void FixedUpdate() {
         rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+
+    void Animate() {
+        animator.SetInteger("AnimState", (int)movement.magnitude);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            animator.SetTrigger("Attack1");
+        }
+    }
+
+    void Flip() {
+        Vector3 newScale = transform.localScale;
+        if (movement.x > 0) {
+            newScale.x = 1f;
+        } else if (movement.x < 0) {
+            newScale.x = -1f;
+        }
+        transform.localScale = newScale;
     }
 }
