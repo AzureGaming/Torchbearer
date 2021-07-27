a1db9d8f41d1393ccb71208cc2616d7938e21b95
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-07-26 20:08:35 -0400

    Add basic camera follow

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Follow : MonoBehaviour {
-    Transform target;
+    public Vector3 offset;
+    public Transform target;
+
+    private void FixedUpdate() {
+        if (target) {
+            transform.position = new Vector3(target.transform.position.x + offset.x, target.transform.position.y + offset.y, target.transform.position.z + offset.z);
+        }
+    }
 }
