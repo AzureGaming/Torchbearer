aa7d5039eead06bc45a2cd3fb8e5c6f7b34d4af0
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-09-02 23:37:04 -0400

    Add gun roll

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour {
     public delegate void BroadSwordSpecialAttack(Vector2 dir, float strength, float time, Action cb);
     public static BroadSwordSpecialAttack OnBroadSwordSpecialAttack;
 
+    public GameObject rollPrefab;
     public Transform dashTarget;
+    public AudioSource roll;
 
     Rigidbody2D rb;
     Animator animator;
@@ -16,6 +18,7 @@ public class Player : MonoBehaviour {
     Vector2 movement;
     float speed = 3f;
     bool isDashing = false;
+    bool isRolling = false;
 
     private void OnEnable() {
         OnBroadSwordSpecialAttack += StartDash;
@@ -46,13 +49,15 @@ public class Player : MonoBehaviour {
             Attack();
         } else if (Input.GetMouseButtonDown(1)) {
             SpecialAttack();
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
+            Roll();
         }
     }
 
     private void FixedUpdate() {
-       if (!isDashing) {
-           rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
-       }
+        if (!isDashing && !isRolling) {
+            rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        }
     }
 
     void Animate() {
@@ -72,6 +77,34 @@ public class Player : MonoBehaviour {
         Weapon.OnSpecialAttack?.Invoke();
     }
 
+    void Roll() {
+        StartCoroutine(RollRoutine());
+    }
+
+    IEnumerator RollRoutine() {
+        if (movement.normalized != Vector2.zero) { // only roll when movement is detected
+            roll.Play();
+            isRolling = true;
+
+            GameObject rollEffect = Instantiate(rollPrefab, transform);
+            rollEffect.transform.up = -movement.normalized;
+
+            float timeElapsed = 0f;
+            float rollTime = 0.15f;
+            Vector2 start = transform.position;
+            Vector2 target = (Vector2)transform.position + movement.normalized * 3f;
+            while (timeElapsed <= rollTime) {
+                Vector2 newPos = Vector2.Lerp(start, target, ( timeElapsed / rollTime ));
+                rb.MovePosition(newPos);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            Destroy(rollEffect);
+            isRolling = false;
+        }
+    }
+
     void StartDash(Vector2 dir, float strength, float time, Action cb) {
         GetComponentInChildren<FaceMouse>().enabled = false;
         GetComponentInChildren<WeaponPosition>().enabled = false;
@@ -84,12 +117,12 @@ public class Player : MonoBehaviour {
         Vector2 destination = dashTarget.position;
         Vector2 start = transform.position;
         while (timeElapsed <= time) {
-          Vector2 newPos = Vector2.Lerp(start, destination, (timeElapsed / time));
-          rb.MovePosition(newPos);
-          timeElapsed += Time.deltaTime;
-          yield return null;
+            Vector2 newPos = Vector2.Lerp(start, destination, ( timeElapsed / time ));
+            rb.MovePosition(newPos);
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
-        
+
         isDashing = false;
         GetComponentInChildren<FaceMouse>().enabled = true;
         GetComponentInChildren<WeaponPosition>().enabled = true;
