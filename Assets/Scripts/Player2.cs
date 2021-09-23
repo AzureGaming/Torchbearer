8a154e23985af49b4cbf20bfc4bae92e02972e7b
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-09-22 22:36:57 -0400

    Fix player clipping into wall

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -14,13 +14,18 @@ public class Player2 : MonoBehaviour {
     public AudioSource dashImpact;
     public ParticleSystem dust;
     public ParticleSystem dashCollisionDust;
+    public Transform dashCollisionPoint;
 
     public float movementSpeed = 3f;
     public float dashSpeed = 2f;
     public float dashCollisionRadius;
     public float dashCollisionForce = 5f;
-    public Transform dashCollisionPoint;
     bool isRolling = false;
+
+    Vector2 start;
+    Vector2 target;
+    float timeElapsed;
+    float rollTime = 0.15f;
 
     private void Awake() {
         animator = GetComponent<Animator>();
@@ -44,6 +49,30 @@ public class Player2 : MonoBehaviour {
     private void FixedUpdate() {
         if (!isRolling) {
             rb.AddForce(movement * movementSpeed);
+        } else if (isRolling && timeElapsed >= rollTime) {
+            isRolling = false;
+            Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, dashCollisionRadius, LayerMask.GetMask(new string[1] { "Dash Attack" }));
+
+            if (collisions.Length > 0) {
+                dashCollisionDust.Play();
+                StartCoroutine(Camera.main.GetComponent<CameraShake>().Shake(0.1f, 0.1f));
+            }
+
+            foreach (Collider2D collision in collisions) {
+                Vector2 direction = collision.transform.position - transform.position;
+                direction.Normalize();
+                Vector2 point = (Vector2)transform.position + direction * dashCollisionRadius;
+                float distanceFromPerimeter = Vector2.Distance(collision.transform.position, point);
+                float force = distanceFromPerimeter * dashCollisionForce;
+
+                PlayDashImpact();
+                collision.GetComponent<DashAttackTarget>().TakeDamage(transform.position, force);
+            }
+
+        } else if (isRolling && timeElapsed < rollTime) {
+            Vector2 newPos = Vector2.Lerp(start, target, ( timeElapsed / rollTime ));
+            rb.MovePosition(newPos);
+            timeElapsed += Time.deltaTime;
         }
     }
 
@@ -67,44 +96,23 @@ public class Player2 : MonoBehaviour {
     }
 
     IEnumerator RollRoutine() {
-        //rb.velocity = Vector2.zero;
-        collider2d.enabled = false;
-        isRolling = true;
-        Vector2 start = transform.position;
-        Vector2 target = (Vector2)transform.position + movement.normalized * dashSpeed;
-        float timeElapsed = 0f;
-        float rollTime = 0.15f;
+        start = transform.position;
+        target = (Vector2)transform.position + movement.normalized * dashSpeed;
+        timeElapsed = 0f;
 
+        isRolling = true;
         slide.time = 2.5f;
         slide.Play();
         dust.Play();
 
-        while ((Vector2)transform.position != target) {
-            Vector2 newPos = Vector2.Lerp(start, target, ( timeElapsed / rollTime ));
-            rb.MovePosition(newPos);
-            timeElapsed += Time.deltaTime;
-            yield return null;
-        }
-        isRolling = false;
+        //while ((Vector2)transform.position != target) {
+        //    Vector2 newPos = Vector2.Lerp(start, target, ( timeElapsed / rollTime ));
+        //    rb.MovePosition(newPos);
+        //    timeElapsed += Time.deltaTime;
+        //    yield return null;
+        //}
 
-        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, dashCollisionRadius, LayerMask.GetMask(new string[1] { "Dash Attack" }));
-
-        if (collisions.Length > 0) {
-            dashCollisionDust.Play();
-            StartCoroutine(Camera.main.GetComponent<CameraShake>().Shake(0.1f, 0.1f));
-        }
-
-        foreach (Collider2D collision in collisions) {
-            Vector2 direction = collision.transform.position - transform.position;
-            direction.Normalize();
-            Vector2 point = (Vector2)transform.position + direction * dashCollisionRadius;
-            float distanceFromPerimeter = Vector2.Distance(collision.transform.position, point);
-            float force = distanceFromPerimeter * dashCollisionForce;
-
-            PlayDashImpact();
-            collision.GetComponent<DashAttackTarget>().TakeDamage(transform.position, force);
-        }
-        collider2d.enabled = true;
+        yield break;
     }
 
     void PlayDashImpact() {
