883e994830c26cf0b60cbe75ffe65681dcf42e9d
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-09-28 23:18:37 -0400

    Add torch flames

--- Assets/LevelChanger.cs
+++ Assets/LevelChanger.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelChanger : MonoBehaviour {
     public Animator animator;
     public string[] levelNames;
+    public AudioSource hauntedVortex;
 
     int levelToLoad;
 
@@ -17,5 +18,20 @@ public class LevelChanger : MonoBehaviour {
     public void OnFadeComplete() {
         string scene = levelNames[levelToLoad];
         SceneManager.LoadScene(scene);
+    }
+
+    public void PlayFadeIn() {
+        PlaySoundInterval(hauntedVortex, 0f, 0.9f);
+    }
+
+    public void PlayFadeOut() {
+        PlaySoundInterval(hauntedVortex, 0f, 0.9f);
+        //PlaySoundInterval(hauntedVortex, 2.40f, 3f);
+    }
+
+    void PlaySoundInterval(AudioSource audio, float start, float end) {
+        audio.time = start;
+        audio.Play();
+        audio.SetScheduledEndTime(AudioSettings.dspTime + ( end - start ));
     }
 }
